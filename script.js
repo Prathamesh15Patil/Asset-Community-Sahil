a37ca9ee9ac37320b7f86ae723ef890975f6c169
PrathameshjPatil <prathameshjpatil15@gmail.com>
2024-08-31 19:50:05 +0530

    Added project files to repo

---
+++ script.js
@@ -0,0 +1,16 @@
+const accordians = document.querySelectorAll('.accordian');
+
+accordians.forEach(accordian => {
+    const icon = accordian.querySelector('.f-icon');
+    const ans = accordian.querySelector('.ans');
+
+    accordian.addEventListener('click',()=>{
+        if(icon.classList.contains('active')){
+            icon.classList.remove('active');
+            ans.style.maxHeight = null;
+        }else{
+            icon.classList.add('active');
+            ans.style.maxHeight = ans.scrollHeight + 'px';
+        }
+    })
+})
